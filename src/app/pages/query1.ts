4fe2b12ee0e9accf449930713afee2ebd2215001
JackyKho0622 <jackykho0622@gmail.com>
2024-10-15 17:26:10 +0800

    deleting unecessary thing

--- src/app/pages/query1.ts
+++ src/app/pages/query1.ts
@@ -1,20 +1,4 @@
 export const queryForQ1 = 
 `data record(id = "EC2_INSTANCE-56ED6F3F544C46E7", entity.name = "loggregator_trafficcontroller/19b46190-a1a6-47c8-99bb-34606e869b0f", awsInstanceType = "t3.micro"),record(id = "EC2_INSTANCE-7F74BA855D16461C", entity.name = "i-0b046099511f2b0d4", awsInstanceType = "t3.xlarge"),record(id = "EC2_INSTANCE-211185EFB8A5A59D", entity.name = "i-0ac16271a92bd4251", awsInstanceType = "t3.xlarge"),record(id = "EC2_INSTANCE-12E75C1A343DAB90", entity.name = "i-0ac7daa0930d5910e", awsInstanceType = "t3.xlarge"),record(id = "EC2_INSTANCE-9328ACB598C4419A", entity.name = "mysql_proxy/d6974a6e-4108-40f7-aa3b-8c1e41fdd55d", awsInstanceType = "t3.micro"),record(id = "EC2_INSTANCE-97A4036C98C107C6", entity.name = "bosh/0", awsInstanceType = "m5.large"),record(id = "EC2_INSTANCE-E567A65166A68BDA", entity.name = "demoOnDemandSaas_ag", awsInstanceType = "m5.large"),record(id = "EC2_INSTANCE-B8B3B489C7BE7469", entity.name = "uaa/6403cd39-aa28-467a-8b4a-f278d20d39a1", awsInstanceType = "m5.large"),record(id = "EC2_INSTANCE-E937EA86DBEEBE2B", entity.name = "i-0e809b95fe1bf256e", awsInstanceType = "t3.xlarge"),record(id = "EC2_INSTANCE-FF5E8C16035D4177", entity.name = "et-demo-2-lnx9", awsInstanceType = "m5.large")
 |fieldsRename ID = id, Name = entity.name, \`AWS Instance Type\` = awsInstanceType`;
-
-// `timeseries cpuAvg = avg(dt.host.cpu.usage), by:{dt.entity.host, host.name}
-// | fieldsRename hostId = dt.entity.host, hostName = host.name
-// | lookup [
-//   timeseries {
-//     idle=avg(dt.host.cpu.idle),
-//     ioWait=avg(dt.host.cpu.iowait),
-//     user=avg(dt.host.cpu.user),
-//     system=avg(dt.host.cpu.system),
-//     steal=avg(dt.host.cpu.steal)
-//   },
-//   by:{dt.entity.host}
-//   | fieldsAdd idle = arrayAvg(idle), ioWait = arrayAvg(ioWait), user = arrayAvg(user), system = arrayAvg(system), steal = arrayAvg(steal)
-//   | fieldsAdd other = 100 - idle - ioWait - user - system - steal
-// ], sourceField:hostId, lookupField:dt.entity.host, fields:{idle, ioWait, user, system, steal, other}`
-
 ;
